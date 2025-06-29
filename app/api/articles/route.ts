ea653dd3b44828461d287107f1961341f8204936
imaginer <mailing.imaginer@gmail.com>
2025-06-29 13:50:08 +0530

    change in the route.ts

--- app/api/articles/route.ts
+++ app/api/articles/route.ts
@@ -22,5 +22,5 @@ export async function GET(req: NextRequest) {
   const response = NextResponse.json(data);
   response.headers.set('Cache-Control', 'no-store, max-age=0');
   return response;
-}
+  }
 
