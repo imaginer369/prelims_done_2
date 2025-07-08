18d740acec42336b3d348b390b8a1f4645e422f1
imaginer <mailing.imaginer@gmail.com>
2025-07-08 19:35:40 +0530

    second

--- pages/quiz/index.tsx
+++ pages/quiz/index.tsx
@@ -1,2 +1,3 @@
+// REMOVE THIS FILE: This file is obsolete and causes a build conflict with app/quiz/page.tsx. The quiz page is now implemented in the app directory. You must delete /pages/quiz/index.tsx and the entire /pages/quiz directory to resolve the build error.
 
-// This file is now obsolete. The quiz page is implemented in the app directory.
+
