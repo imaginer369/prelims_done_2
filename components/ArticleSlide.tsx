907a36ffa464b768d36ca2a77f545452bba49098
imaginer <mailing.imaginer@gmail.com>
2025-07-12 15:35:34 +0530

    scroll full article

--- components/ArticleSlide.tsx
+++ components/ArticleSlide.tsx
@@ -63,7 +63,7 @@ export default function ArticleSlide({ article, forceFullContent = false }: Arti
             </div>
           )}
           {/* Article Content */}
-          <div className="flex-1 flex flex-col p-6 sm:p-10 overflow-y-auto">
+          <div className="flex-1 flex flex-col p-6 sm:p-10 overflow-y-auto max-h-[80vh]">
             <h1 className="text-3xl sm:text-4xl font-extrabold text-gray-900 dark:text-blue-200 mb-2 leading-tight drop-shadow-sm">
               {article.title}
             </h1>
