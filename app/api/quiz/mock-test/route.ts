1038ffbe0a5b60715aa5c55e53942607b13244a7
imaginer <mailing.imaginer@gmail.com>
2025-08-09 04:26:45 +0530

    second user id in mock

--- app/api/quiz/mock-test/route.ts
+++ app/api/quiz/mock-test/route.ts
@@ -1,5 +1,4 @@
 import { NextRequest } from "next/server";
-import { cookies } from "next/headers";
 import { supabase } from "@/lib/supabaseClient";
 import { difficultyMap, topicMap } from "@/lib/quizMappings";
 
